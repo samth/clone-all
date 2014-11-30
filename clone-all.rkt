c4ce453b8e357ad22dd7579927e6caa44b41aa70
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2014-11-30 14:55:32 -0500

    initial commit

---
+++ clone-all.rkt
@@ -0,0 +1,44 @@
+#lang racket/base
+
+(require pkg pkg/lib setup/setup)
+
+(define default '("~a" "~a-lib" "~a-doc"))
+
+(define all-pkgs
+  (hash "distro-build" '("~a" "~a-lib" "~a-client" "~a-server")
+        "remote-shell" default
+        "plt-web" default
+        "racket-lang-org" #f
+        "pkg-build" #f
+        "pkg-push" #f
+        "games" #f
+        "drdr" #f
+        "drdr2" #f))
+
+(define (gen-all-pkgs p sub)
+  (if sub
+      (for/list ([fmt sub])
+        (format fmt p))
+      (list p)))
+
+(module+ main
+  (require racket/cmdline)
+  (define setup-at-end #f)
+  (define setup? #t)
+  (define run? #f)
+
+  (command-line
+   #:program "clone-all"
+   #:once-any
+   [("--setup-at-end") "Run `raco setup' once at the end" (set! setup-at-end #t)]
+   [("--no-setup") "Don't run `raco setup' at all" (set! setup? #f)])
+
+  (for ([(p sub) all-pkgs])
+    (define ps (gen-all-pkgs p sub))
+    (unless (or (directory-exists? p)
+                (ormap pkg-directory ps))
+      (set! run? #t)
+      (apply pkg-install-command #:no-setup (if setup? setup-at-end #t)  #:clone p ps)))
+  (when (and run? setup? setup-at-end)
+    (setup)))
+  
